2276fff5b79f1be2845cd3f470dfcfa91a915f9c
Ardhi Lukianto <ardhi@lukianto.com>
2018-07-28 10:44:24 +0700

    reg update

---
+++ cuks/core/helper/get/id_column.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = function(cuk) {
+  const { _, helper } = cuk.pkg.core.lib
+
+  return name => {
+    const collName = _.snakeCase(name)
+    return helper('model:getDab')(name).collection[collName].srcAttribId
+  }
+}
